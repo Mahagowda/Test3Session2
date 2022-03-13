c9b1270907983165255a2e353a537d72c9f3511e
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2022-03-13 08:33:26 +0000

    p4

--- p4final.c
+++ p4final.c
@@ -1,24 +1,44 @@
-#include <stdio.h>
-
+#include<stdio.h>
+int input_degree()
+{
+  int n;
+  printf("input degree?");
+  scanf("%d",&n);
+  return n+1;
+}
+float input_x()
+{
+  float x;
+  printf("enter x?");
+  scanf("%f",&x);
+  return x;
+}
+void input_coefficent(int n,float a[n])
+{
+  printf("input %d coefficents-\n",n);
+  for(int i=0;i<n;i+=1)
+    scanf("%f",&a[i]);
+}
+float evaluate_polynomial(int n, float a[n],float x)
+{
+  float rs=a[0];
+  for (int i=1;i<n;i+=1)
+    rs=rs*x+a[i];
+  return rs;
+}
+void output(int n,float a[n], float x,float result)
+{
+  printf("\n");
+  for(int i=0;i<n-1;i+=1)
+    printf("%f x^%d +",a[i],n-i-1);
+  printf("%f,at x=%f\n",a[n-1],x);
+  printf("=%f\n",result);
+}
 int main()
 {
-float a[100],sum=0,x;
-int n,i;
-printf("\nEnter degree of the polynomial X :: ");
-scanf("%d",&n);
-printf("\nEnter coefficient's of the polynomial X :: \n");
-for(i=n;i>=0;i--)
-{
-printf("\nEnter Coefficient of [ X^%d ] :: ",i);
-scanf("%f",&a[i]);
-}
-printf("\nEnter the value of X :: ");
-scanf("%f",&x);
-for(i=n;i>0;i--)
-{
-sum=(sum+a[i])*x;
-}
-sum=sum+a[0];
-printf("\nValue of the polynomial is = [ %f]\n",sum);
-return 0;
+  int n= input_degree();
+  float x=input_x();
+  float ar[n];
+  float_coefficents(n,ar);
+  output(n,ar,x,evaluate_polynomial(n,ar,x));
 }
