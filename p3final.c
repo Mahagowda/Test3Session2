f383f1da83edf78c8b11f905bd1186d902cb09f5
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2022-03-27 11:33:34 +0000

    p3

--- p3final.c
+++ p3final.c
@@ -1,36 +1,36 @@
 #include<stdio.h>
 void input_n_and_r(int *n,int *r)
 {
-  printf("enter the total number of objects and the number of objects you choosen\n");
-  scanf("%d%d",n,r);
+ printf("enter the total number of objects and the number of objects you choosen\n");
+ scanf("%d%d",n,r);
 }
 int ncr(int n,int r)
 {
-  int i,fact_n=1,fact_r=1,fact_n_r=1,ncr;
-  for (i=1;i<=n;i++)
+ int i,fact_n=1,fact_r=1,fact_n_r=1,ncr;
+ for (i=1;i<=n;i++)
   {
-    fact_n=fact_n*i;
+ fact_n=fact_n*i;
   }
-  for(i=1;i<=r;i++)
+ for(i=1;i<=r;i++)
   {
-    fact_r=fact_r*i;
+ fact_r=fact_r*i;
   }
-  for(i=1;i<=n-r;i++)
+ for(i=1;i<=n-r;i++)
   {
-  fact_n_r=fact_n_r*i;
+ fact_n_r=fact_n_r*i;
   }
-  ncr=fact_n/(fact_r*fact_n_r);
+ ncr=fact_n/(fact_r*fact_n_r);
   return ncr;
 }
 void  output(int n,int r,int result)
 {
-  printf("the combination of %d and %d is %d",n,r,result);
+printf("the combination of %d and %d is %d",n,r,result);
 }
 int main()
 {
-  int n,r,result;
-  input_n_and_r(&n,&r);
-  result= ncr(n,r);
-  output(n,r,result);
-  return 0;
+ int n,r,result;
+ input_n_and_r(&n,&r);
+ result= ncr(n,r);
+ output(n,r,result);
+ return 0;
 }
