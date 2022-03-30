724d0f10ad2ebed9b68d3bd693dbd64b7ab012aa
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2022-03-30 18:52:52 +0000

    p2

--- p2final.c
+++ p2final.c
@@ -18,11 +18,11 @@ int num1=f1.num* common_denominator/f1.den;
 int num2=f2.num* common_denominator/f2.den;
 int num3=f3.num* common_denominator/f3.den;
 if(num1< num2 && num1<num3)
-  return f1;
-else if(num2<num3)
-  return f2;
-else
-  return f3;
+   return f1;
+ else if(num2<num3)
+   return f2;
+ else
+   return f3;
 }
 void output(Fraction f1,Fraction f2,Fraction f3,Fraction smallest)
 {
@@ -30,11 +30,11 @@ printf("the smallest of%d%d,%d%d,%d%d is  %d%d\n",f1.num,f1.den,f2.num,f2.den,f3
 }
 int main()
 {
-Fraction f1,f2,f3,f4,smallest;
-f1=input_fraction();
-f2=input_fraction();
-f3=input_fraction();
-smallest=smallest_fraction(f1,f2,f3);
-output(f1,f2,f3,smallest);
-return 0;
+ Fraction f1,f2,f3,f4,smallest;
+ f1=input_fraction();
+ f2=input_fraction();
+ f3=input_fraction();
+ smallest=smallest_fraction(f1,f2,f3);
+ output(f1,f2,f3,smallest);
+ return 0;
 }
