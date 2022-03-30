3a48ae4a239cae6236520c45319b1b516445b7bc
Mahagowda <94665876+Mahagowda@users.noreply.github.com>
2022-03-30 18:48:14 +0000

    p3

--- p3final.c
+++ p3final.c
@@ -8,17 +8,17 @@ int ncr(int n,int r)
 {
  int i,fact_n=1,fact_r=1,fact_n_r=1,ncr;
  for (i=1;i<=n;i++)
-  {
+{
  fact_n=fact_n*i;
-  }
+}
  for(i=1;i<=r;i++)
-  {
+{
  fact_r=fact_r*i;
-  }
+}
  for(i=1;i<=n-r;i++)
-  {
+{
  fact_n_r=fact_n_r*i;
-  }
+}
  ncr=fact_n/(fact_r*fact_n_r);
   return ncr;
 }
@@ -28,9 +28,9 @@ printf("the combination of %d and %d is %d",n,r,result);
 }
 int main()
 {
- int n,r,result;
- input_n_and_r(&n,&r);
- result= ncr(n,r);
- output(n,r,result);
- return 0;
+int n,r,result;
+input_n_and_r(&n,&r);
+result= ncr(n,r);
+output(n,r,result);
+return 0;
 }
